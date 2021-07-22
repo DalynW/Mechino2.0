ae69b267f35a1005b9720ef521f1134ae9e01d84
DalynW <dalyn.1998@gmail.com>
2021-07-22 13:33:05 -0600

    Basic navigation

--- mechino_main/urls.py
+++ mechino_main/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.user_login, name='login'),
-    path('home/', views.user_home, name='home'),
+    path('user/', views.user_home, name='user_home'),
+    path('signup/', views.user_signup, name='user_signup'),
+    path('user/edit/', views.user_edit, name='user_edit'),
+    path('user/vehicles/', views.vehicle_view, name='vehicle_view'),
+    path('user/vehicles/add/', views.user_add_vehicle, name='vehicle_add'),
+    path('user/vehicles/edit/', views.user_edit_vehicle, name='vehicle_edit'),
 ]
--- mechino_main/views.py
+++ mechino_main/views.py
@@ -4,27 +4,36 @@ from django.http import HttpResponse
 
 # Home page of the website
 def index(request):
-    return HttpResponse('Hello this is the index of Mechino!')
+    return render(request, 'mechino/index.html')
 
 # User login page
 def user_login(request):
-    return HttpResponse('Hello this is the user login')
+    return render(request, 'mechino/login.html')
 
 # User home page for after the log in.
 # This will also contain a list of their vehicles and have the option to add or remove vehicles
 def user_home(request):
-    return HttpResponse('Hello this is the user home')
+    return render(request, 'mechino/userHome.html')
 
 # User creation for new accounts
 def user_signup(request):
-    return HttpResponse('User Creation')
+    return render(request, 'mechino/signup.html')
 
 # User info editing
 # change phone-number and/or email
 def user_edit(request):
-    return HttpResponse('User Editing')
+    return render(request, 'mechino/userEdit.html')
+
+# User vehicle view
+# Better view of their vehicles
+def vehicle_view(request):
+    return render(request, 'mechino/vehicleView.html')
 
 # Add vehicle page for entering vehicle details DALYN WAS HERE!!!!!
 def user_add_vehicle(request):
-    return HttpResponse('User add vehicle')
+    return render(request, 'mechino/vehicleAdd.html')
+
+#edit a vehicle
+def user_edit_vehicle(request):
+    return render(request, 'mechino/vehicleEdit.html')
 
